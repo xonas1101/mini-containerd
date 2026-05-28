777319692cfeb7d8a1d3fa3debe4cf8ee23ea9f2
xonas1101 <aarushsingh1305@gmail.com>
2026-05-28 12:41:30 +0530

    added helpers
    Signed-off-by: xonas1101 <aarushsingh1305@gmail.com>

---
+++ container.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	"github.com/thanhpk/randstr"
+)
+
+type Container struct {
+	ID        string    `json:"id"`
+	Status    string    `json:"status"` // "created", "running", "stopped", "exited"
+	PID       int       `json:"pid"`
+	Image     string    `json:"image"`
+	Command   []string  `json:"command"`
+	CreatedAt time.Time `json:"created_at"`
+	ExitCode  int       `json:"exit_code"`
+}
+
+func NewContainer(image string, command []string) *Container {
+	c := &Container{
+		ID:        randstr.String(16),
+		Status:    "created",
+		Image:     image,
+		Command:   command,
+		CreatedAt: time.Now(),
+	}
+	return c
+}
+
+func writeNewContainerOnDisk(c *Container) error {
+	dir := "/var/lib/mini-docker/containers/" + c.ID
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Printf("failed to create container directory %s: %v", dir, err)
+		return err
+	}
+
+	f, err := os.Create(dir + "/config.json")
+	if err != nil {
+		log.Printf("failed to create config.json for container %s: %v", c.ID, err)
+		return err
+	}
+	defer f.Close()
+
+	if err := json.NewEncoder(f).Encode(c); err != nil {
+		log.Printf("failed to encode container %s to disk: %v", c.ID, err)
+		return err
+	}
+
+	log.Printf("container %s written to disk", c.ID)
+	return nil
+}
+
+func (c *Container) Start() {
+	c.Status = "running"
+	log.Printf("container %s started", c.ID)
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.22.2
 
 require github.com/opencontainers/runtime-spec v1.3.0
 
-require github.com/urfave/cli/v3 v3.9.0 // indirect
+require (
+	github.com/thanhpk/randstr v1.0.6 // indirect
+	github.com/urfave/cli/v3 v3.9.0 // indirect
+)
